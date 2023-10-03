db73dd56c847c1a044de9dbd079ffde29f16f615
Dereck Palacios <dpalaciosurbi@gmail.com>
2023-10-03 17:47:31 -0600

    comenzando el ejercicio

--- MetodosSobrecargados/src/App.java
+++ MetodosSobrecargados/src/App.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 public class App {
-    static final double Taza_Antiguedad = 0.15;
+    static final float Taza_Antiguedad = 0.15F;
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         System.out.println("Ingrese su salario bruto:  ");
-        double ingreso_Bruto=scan.nextInt();
+        float ingreso_Bruto=scan.nextInt();
         Ingresos(ingreso_Bruto);
     }
-    public static void Ingresos(double ingreso_Bruto){
-        double ingreso_Anti = Taza_Antiguedad * ingreso_Bruto;
+    public static void Ingresos(float ingreso_Bruto){
+        float ingreso_Anti = Taza_Antiguedad * ingreso_Bruto;
+        float ingreso_total = ingreso_Bruto + ingreso_Anti;
+        // deduccionesTotales = Deducciones();
+        // int ingreso_neto = ingreso_total - deduccionesTotales;
 
-        int ingreso_total = ingreso_Bruto + ingreso_Anti;
-        deduccionesTotales = Deducciones();
-        int ingreso_neto = ingreso_total - deduccionesTotales;
-
-        int ingreso_Anti = ingreso_Bruto;
-        Deducciones(ingreso_Anti);
+        // int ingreso_Anti = ingreso_Bruto;
+        
     }
 
-    public static void Deducciones(int ingreso_Anti){
-        double result = ingreso_Anti * Taza_Antiguedad;
+    public static void Deducciones(){
+        
     }
     
 }
